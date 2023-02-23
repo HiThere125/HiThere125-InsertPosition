89a6d17752bda65c567e0727e6743599a5ae6a03
Brent Kimura <bkimura73@gmail.com>
2023-02-23 09:00:04 +0000

    Completed Insert Position Challenge

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,16 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        if(nums.length == 1){
+            if(target < nums[0]){
+                return 0;
+            }
+        }
+        for(int i = 0; i < nums.length-1; i++){
+            if(target > nums[i] && target < nums[i+1]){
+                return i+1;
+            }
+        }
+        return nums.length;
     }
 }
